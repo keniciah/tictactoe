33775ba9bde83a4781ded415a7173e63b08ca571
keniciah <keniciah@gmail.com>
2017-09-04 17:00:07 -0500

    Add files via upload

---
+++ TestTicTacToe.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TestTicTacToe;
+
+/**
+ *
+ * @author kenicia
+ */
+public class TestTicTacToe {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        TicTacToeGUI load = new TicTacToeGUI();
+        load.setVisible(true);
+    }
+}
